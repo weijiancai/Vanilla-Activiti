37bdb5f262fedd14dd26f2d7197b85a2c663fb3e
wei_jc <weijiancai@126.com>
2018-03-06 11:03:32 +0800

    移除activiti-modelorer,挪到了com.ectongs.activiti.editor，web-xml中配置spring-mvc

--- src/main/java/com/ectongs/activiti/editor/StencilsetRestResource.java
+++ src/main/java/com/ectongs/activiti/editor/StencilsetRestResource.java
@@ -1,13 +1,15 @@
-package com.ectongs.activiti.servlet;
+package com.ectongs.activiti.editor;
 
 import org.activiti.engine.ActivitiException;
 import org.apache.commons.io.IOUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.InputStream;
 
+@RestController
 public class StencilsetRestResource {
     @RequestMapping(value="/editor/stencilset", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
     public @ResponseBody String getStencilset() {
